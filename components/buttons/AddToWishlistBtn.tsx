ff5576fa847b05c4b563f2a40c89a60097c660bc
kashershahzad <kashershahzadprogramming@gmail.com>
2025-03-13 14:37:19 +0500

    vercel issue fixed

--- components/buttons/AddToWishlistBtn.tsx
+++ components/buttons/AddToWishlistBtn.tsx
@@ -8,17 +8,17 @@ import { showToast } from "@/lib/showToast";
 const AddToWishlistBtn = ({product}:{product:Product}) => {
   const {addToWishlist,isInWishlist} = useWishlistStore()
 
-  const handleAddToWishList = () => {
-    if(isInWishlist(product.id)){
-      showToast('Item Already Exist In Wishlist',product.images[0] as string, product.name)
-    }else{
-      addToWishlist(product);
-    showToast('Item Added To The Wishlist',product.images[0] as string, product.name)
-    }
-  }
+  // const handleAddToWishList = () => {
+  //   if(isInWishlist(product.id)){
+  //     showToast('Item Already Exist In Wishlist',product.images[0] as string, product.name)
+  //   }else{
+  //     addToWishlist(product);
+  //   showToast('Item Added To The Wishlist',product.images[0] as string, product.name)
+  //   }
+  // }
 
   return (
-    <Button onClick={(handleAddToWishList)} variant={"outline"} className="w-full p-8 text-xl rounded-full">
+    <Button  variant={"outline"} className="w-full p-8 text-xl rounded-full">
       {" "}
       Add To Wishlish
     </Button>
