ec106f3401f4b889ded76efca6bd0e33ca4d27b2
kashershahzad <kashershahzadprogramming@gmail.com>
2025-03-16 23:14:05 +0500

    footer integrated

--- components/footers/Footer.tsx
+++ components/footers/Footer.tsx
@@ -1,13 +1,15 @@
-import React from "react";
+import { useState } from "react";
 import { Separator } from "../ui/separator";
-import { FaFacebook, FaTwitter } from "react-icons/fa6";
+import { FaFacebook, FaTwitter, FaGithub, FaLinkedin , FaInstagram } from "react-icons/fa6";
 import { FaInstagramSquare } from "react-icons/fa";
 import Link from 'next/link'
-import { dummyCategories } from "@/data/category/categoryData";
 import Logo from "../logo/Logo";
+import useShopifyCollections from "@/hooks/useShopifyCollections";
 
 
 const Footer = () => {
+  const { collections, loading, error } = useShopifyCollections();
+  console.log("footer:", collections);
   return (
     <footer className=" bg-gray-700 text-white py-8 px-4 md:px-8">
       <div className="max-w-screen-xl mx-auto p-4 md:p-8 flex md:flex-row  flex-wrap gap-4 md:gap-2 justify-between">
@@ -16,110 +18,71 @@ const Footer = () => {
           <p>Your one-stop shop for all things electronics.</p>
           <div className="flex space-x-4">
             <Link
-              href="www.facebook.com"
+              href="https://www.linkedin.com/in/kasher-shahzad-223579288/"
               className=""
             >
-              <FaFacebook size={30}/>
+              <FaLinkedin size={30} />
             </Link>
             <Link
-              href="www.x.com"
+              href="https://www.instagram.com/kasher_shahzad/"
               className=""
             >
-              <FaTwitter size={30}/>
+              <FaInstagram size={30} />
             </Link>
             <Link
-              href="www.instagram.com"
+              href="https://github.com/kashershahzad"
               className=""
             >
-              <FaInstagramSquare size={30}/>
+              <FaGithub size={30} />
             </Link>
           </div>
         </div>
         <div className="flex flex-col space-y-4">
           <h3 className="text-xl font-semibold">Categories</h3>
           <ul className="space-y-2">
-            {dummyCategories.map(category => (
-              <li key={category.name}>
-              <Link
-                href={`/shop?category=${category.name}`}
-                className=""
-              >
-                {category.name}
-              </Link>
-            </li>
-            ))}
+
+            {collections.map((collection) => {
+              // Skip rendering if the collection handle is "feature-products"
+              if (collection.handle === "feature-products") {
+                return null;
+              }
+
+              // Log the handle for each collection
+              console.log("Collection Handle:", collection.handle);
+
+              // Check if the collection handle is "mob" and change the title to "Smartphones"
+              const collectionTitle = collection.handle === "mob" ? "Smartphones" : collection.title;
+
+              return (
+                <li key={collection.id}>
+                <Link
+                  href={`/shop?category=${collection.handle}`}
+                  className=""
+                >
+                  {collectionTitle}
+                </Link>
+                </li>
+              );
+            })}
           </ul>
         </div>
         <div className="flex flex-col space-y-4">
-          <h3 className="text-xl font-semibold">Navigation</h3>
+          <h3 className="text-xl font-semibold">About Website</h3>
           <ul className="space-y-2">
             <li>
               <Link
                 href="/"
                 className=""
               >
-                Home
+                Docometation
               </Link>
             </li>
             <li>
               <Link
-                href="/about"
+                href="https://github.com/kashershahzad/Ecommerce-Website-Shopify-Backend-Frontend-Headless-CMS"
                 className=""
               >
-                About Us
-              </Link>
-            </li>
-            <li>
-              <Link
-                href="/contact"
-                className=""
-              >
-                Contact Us
-              </Link>
-            </li>
-            <li>
-              <Link
-                href="/shop"
-                className=""
-              >
-                Shop
-              </Link>
-            </li>
-          </ul>
-        </div>
-        <div className="flex flex-col space-y-4">
-          <h3 className="text-xl font-semibold">Support</h3>
-          <ul className="space-y-2">
-            <li>
-              <Link
-                href="/help"
-                className=""
-              >
-                Help Center
-              </Link>
-            </li>
-            <li>
-              <Link
-                href="#"
-                className=""
-              >
-                Returns & Refunds
-              </Link>
-            </li>
-            <li>
-              <Link
-                href="#"
-                className=""
-              >
-                Terms of Service
-              </Link>
-            </li>
-            <li>
-              <Link
-                href="#"
-                className=""
-              >
-                Privacy Policy
+                Github
               </Link>
             </li>
           </ul>
@@ -127,7 +90,8 @@ const Footer = () => {
       </div>
       <hr className="w-full h-[2px] bg-white" />
       <div className="text-center mt-8">
-        <p>&copy; 2024 Your Brand Name. All Rights Reserved.</p>
+
+        <p> &copy; 2025 Developed by <Link href="https://www.kashershahzad.live/"><span className="font-bold">Kasher Shahzad</span></Link>. All Rights Reserved.</p>
       </div>
     </footer>
   );
